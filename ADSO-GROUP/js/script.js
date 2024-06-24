dfadf72f122b2aabaef4c19b87bd8169218e7d51
BVargasTapias <brayanvargas0710@gmail.com>
2024-06-24 11:52:46 -0500

    cambiosjs

--- ADSO-GROUP/js/script.js
+++ ADSO-GROUP/js/script.js
@@ -3,4 +3,21 @@ $(document).ready(function () {
     $("#primero").click(function () {
         $("#cont").slideDown()
     })
+    $("#primero").dblclick(function () {
+        $("#cont").slideUp()
+    })
+    $("#prueba").hide()
+    $("#prueba1").hide()
+    $("#prueba2").hide()
+    $("#tres").hover(function () {
+        $("#prueba").slideDown()
+        $("#prueba1").slideDown()
+        $("#prueba2").slideDown()
+    })
+
 })
+
+$(document).ready(function () {
+
+})
+
